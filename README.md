5bf8d0ace8f85b3e0d35d25427c4daecb9129b8b
Burak Sekili <buraksekili@gmail.com>
2023-07-20 14:22:02 +0300

    Initial commit
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+```shell
+copins --source example-com-tls --source-ns staging --dest-ns prod --dest example-com-tls-prod
+```
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"flag"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+var (
+	sourceSecretName      = flag.String("source", "", "Name of the source secret")
+	sourceSecretNamespace = flag.String("source-ns", "default", "Namespace of the source secret")
+	destSecretName        = flag.String("dest", "", "Name of the destination secret")
+	destSecretNamespace   = flag.String("dest-ns", "default", "Namespace of the destination secret")
+)
+
+func main() {
+	flag.Parse()
+
+	cl := initClient()
+
+	if *destSecretName == "" {
+		*destSecretName = *sourceSecretName
+	}
+
+	secret, err := cl.CoreV1().Secrets(*sourceSecretNamespace).Get(context.Background(), *sourceSecretName, metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	copiedData := copyMap(secret.Data)
+	dstSecret := v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: *destSecretName, Namespace: *destSecretNamespace},
+		Data:       copiedData,
+	}
+
+	// Instead of creating the secret directly, use CreateOrUpdate
+	_, err = cl.CoreV1().Secrets(*destSecretNamespace).Create(context.Background(), &dstSecret, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initClient() *kubernetes.Clientset {
+	clientset := kubernetes.NewForConfigOrDie(config.GetConfigOrDie())
+
+	return clientset
+}
+
+func copyMap(original map[string][]byte) map[string][]byte {
+	// Create a new map to hold the copied data
+	copiedMap := make(map[string][]byte)
+
+	// Iterate over the original map
+	for key, value := range original {
+		// Create a new byte slice to store the copied value
+		copiedValue := make([]byte, len(value))
+		copy(copiedValue, value)
+
+		// Add the key-value pair to the copied map
+		copiedMap[key] = copiedValue
+	}
+
+	return copiedMap
+}
